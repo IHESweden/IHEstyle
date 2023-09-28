a11f531fa385622b62cd760729a32a4fdcb86653
Gunnar Brådvik <76434717+GB-IHE@users.noreply.github.com>
2023-09-28 11:28:47 +0200

    Back to original...

--- R/IHE_style_2023.r
+++ R/IHE_style_2023.r
@@ -1,9 +1,6 @@
 # The ggplot2 function for the IHE style from 2023 -------------------------
 #' @export
-  IHEstyle2023 <- function() {
-    extrafont::font_import(pattern = "trebuc.ttf", prompt = F) # Load the fonts
-	#extrafont::loadfonts(device = "win")
-  
+  IHEstyle2023 <- function() {  
     return(
       list(
         ggplot2::theme( #list combining the theme itself and some settings for the plot
